a1afd4c8059cc7cd36880c3c1e2a26bfb8580bc2
qngxl <hagots@gmail.com>
2024-07-09 19:29:23 +0200

    new file for titled container

--- lib/team_container.dart
+++ lib/team_container.dart
@@ -4,6 +4,7 @@ import 'package:canasta/team.dart';
 import 'package:flutter/material.dart';
 
 import 'checkbox_container.dart';
+import 'titled_container.dart';
 
 class TeamContainer extends StatefulWidget {
   const TeamContainer({super.key, required this.team});
@@ -125,48 +126,6 @@ class TeamContainerState extends State<TeamContainer> {
           )
         ],
       ),
-    );
-  }
-}
-
-class TitledContainer extends StatelessWidget {
-  const TitledContainer(
-      {required this.titleText, required this.child, this.idden = 8, Key? key})
-      : super(key: key);
-  final String titleText;
-  final double idden;
-  final Widget child;
-
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Container(
-          margin: const EdgeInsets.fromLTRB(10, 20, 10, 10),
-          padding: EdgeInsets.all(idden),
-          decoration: BoxDecoration(
-            border: Border.all(),
-            borderRadius: BorderRadius.circular(idden * 0.6),
-          ),
-          child: child,
-        ),
-        Positioned(
-          left: 10,
-          right: 10,
-          top: 10,
-          child: Align(
-            // alignment: Alignment.center,
-            child: Container(
-              color: Colors.white,
-              child: Text(
-                titleText,
-                overflow: TextOverflow.ellipsis,
-                style: const TextStyle(fontSize: 19),
-              ),
-            ),
-          ),
-        ),
-      ],
     );
   }
 }
